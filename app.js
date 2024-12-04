68ed2f62f1cf0928c14355b6963d61738975d45d
sd45g <dosrahplay@gmail.com>
2024-12-04 20:57:51 +0200

    Update app.js with new changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://almohra-frontend-sylf.vercel.app"],
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
